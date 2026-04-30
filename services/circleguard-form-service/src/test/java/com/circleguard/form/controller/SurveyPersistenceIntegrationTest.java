2c627d727fecf077ec8086e17b0dc8d340bb1d06
Electromayonaise <agazazaca@gmail.com>
2026-04-29 21:44:43 -0500

    fix: tag SurveyPersistenceIntegrationTest as integration, exclude from dev unit test stage

--- services/circleguard-form-service/src/test/java/com/circleguard/form/controller/SurveyPersistenceIntegrationTest.java
+++ services/circleguard-form-service/src/test/java/com/circleguard/form/controller/SurveyPersistenceIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.circleguard.form.controller;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Testcontainers
+@Tag("integration")
 class SurveyPersistenceIntegrationTest {
 
     @Container
